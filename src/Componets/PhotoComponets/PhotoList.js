ebf5e4bf0bc0052a68a60c8015768a4632e3e4d2
ddelfaus <ddelfaus@gmail.com>
2019-10-09 19:02:07 -0400

    Added Date and Title

--- src/Componets/PhotoComponets/PhotoList.js
+++ src/Componets/PhotoComponets/PhotoList.js
@@ -6,7 +6,8 @@ import axios from "axios";
  function PhotoList() {
 
 const [photo, setPhoto] = useState({});
-
+const [date, setDate] = useState([])
+const [title, setTitle] = useState([])
   useEffect(() => {
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=ZWLD7HPmhALpwuBx8dQfhBxCgarQGE8uMtthChW5`)
@@ -14,6 +15,8 @@ const [photo, setPhoto] = useState({});
         console.log(response);
         setPhoto(response.data.url);
         console.log(photo)
+        setDate(response.data.date)
+        setTitle(response.data.title)
       })
       .catch(error => {
         console.log("The data was not returned", error);
@@ -21,11 +24,12 @@ const [photo, setPhoto] = useState({});
   }, []);
 
   return ( 
+     
       <div className = "Photo-container">
-   
-     { <img ClassName="Nasa-img" alt= "random space img" src= {photo} /> }
+         {<h1>{title}</h1>}
+     { <img className="Nasa-img" alt= "random space img" src= {photo} /> }
 
-
+        {<h1 className ="date">{date}</h1>}
       </div>
    
   );
